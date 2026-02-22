8308ca8769cef0e5f554ecc983d8f80a308980b5
teodoramorosan <tmorosan093@gmail.com>
2026-02-22 22:30:59 +0200

    feat: enhance task manager interface with additional options and fix display issues

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ class Task:
           status = "✔" if task.completed else "✘"
           print(f"\nIndex: {i}")
           print(f"TITLU: {task.title}")
-          print(f"Descriere: {task>description}")
-          print(f"Print: {task.priority}")
+          print(f"Descriere: {task.description}")
+          print(f"Prioritate: {task.priority}")
           print(f"Status: {status}")
           print(f"Creat la: {task.created_at}")
 
@@ -91,4 +91,58 @@ class Task:
     def main_menu():
         manager = TaskManager()
 
-    
+        while True:
+           print("\n===== TASK MANAGER =====")
+           print("1. Adaugă task")
+           print("2. Șterge task")
+           print("3. Marchează task ca finalizat")
+           print("4. Afișează toate task-urile")
+           print("5. Afișază toate task-uri finalizate")
+           print("6. Afișează task-uri nefinalizate")
+           print("7. Ieșire")
+
+           choice = input("Alege o opțiune: ")
+
+           if choice == "1"
+              title = input("Titlu: ")
+              description = input("Descriere: ")
+              priority = input("Prioritate (Low/Medium/High): ")
+              manager.add_task(title, description, priority)
+
+           elif choice == "2":
+               manager.show_tasks()    
+               try:
+                  index = int(input("Index task de șters: "))
+                  manager.mark_completed(index)
+                except ValueError:
+                    print("Introdu un număr valid!")
+
+            elif choice =="3":
+                manager.show_tasks()
+                try:
+                   index =int(input("Index task de marcat: "))
+                   manager.mark_completed(index)
+                except ValueError:
+                   print("Introdu un număr valid!")
+
+            elif choice == "4":
+                manager.show_tasks()
+
+            elif choice == "5":
+                manager.show_tasks(filter_type="completed")
+
+            elif choice == "6":
+                manager.show_tasks(filter_type="pending")
+
+            elif choice == "7":
+              print("La revedere!")
+              break
+            
+            else:
+              print("Opțiune invalidă!")
+
+            input("\nApasă Enter pentru a continua...")
+            clear_screen()
+
+if__name__ == "__main__":
+   main_menu()
