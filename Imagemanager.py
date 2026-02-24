e27ba1c793fde9dd368af02372412d75972276a8
teodoramorosan <tmorosan093@gmail.com>
2026-02-24 23:13:01 +0200

    feat: implement menu options for image management functionality

--- Imagemanager.py
+++ Imagemanager.py
@@ -68,4 +68,54 @@ class ImageManager:
 
         while True:
             print("====== IMAGE MANAGER ======")
-     
+            print("1. Încarcă imagine")
+            print("2. Info imagine")
+            print("3. Redimensionează")
+            print("4. Grayscale")
+            print("5. Decupează")
+            print("6. Salvează")
+            print("7. Istoric")
+            print("8. Ieșire")
+
+            opt = input("Alege opțiunea: ")
+
+        if opt == "1":
+            path = input("Cale imagine:")
+            manager.incarca_imagine(path)
+
+        elif opt == "2":
+            manager.info_imagine()
+
+        elif opt == "3":
+        l = int(input("Lățime nouă:"))
+        h = int(input("Înălțime nouă:"))
+        manager.redimensioneaza(l, h)
+
+        elif opt =="4":
+            manager.grayscale()
+
+        elif opt  == "5":
+            s = int(input("Stânga: "))
+            j = int(input("Sus: "))
+            d = int(input("Dreapta:"))
+            jos = int(input("Jos: "))
+            manager.decupeaza(s, j, d, jos)
+
+        elif opt == "6":
+            nume = input("Nume fișier nou ("ex: editat.jpg): ")
+            manager.salveaza(nume)
+
+        elif opt =="7":
+            manager.arata_istoric()
+
+        elif opt =="8":
+            print("La revedere!")
+            break
+            
+        else:
+            print("Optiune invalidă!")
+
+
+if __name__ == "__main__":
+    meniu()
+      
