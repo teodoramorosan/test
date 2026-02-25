bc9e012d6d60b21eb0d90cf7c372c29644805a8b
teodoramorosan <tmorosan093@gmail.com>
2026-02-25 14:50:15 +0200

    feat: implement main menu for Life OS application

---
+++ LifeOS/main.py
@@ -0,0 +1,30 @@
+from tasks import *
+from goals import *
+from habits import *
+from finance import *
+
+def menu():
+    print("\n=== LIFE OS ===")
+    print("1. Task Manager")
+    print("2. Goals")
+    print("3. Habits")
+    print("4. Finance")
+    print("5. Life Score")
+    print("0. Exit")
+
+while True:
+    menu()
+    choice = input("Choose option: ")
+
+    if choice == "1":
+        task_menu()
+    elif choice == "2":
+        goal_menu()
+    elif choice == "3":
+        habit_menu()
+    elif choice == "4":
+        finance_menu()
+    elif choice == "5":
+        print("Life Score:", calculate_life_score())
+    elif choice == "0":
+        break
