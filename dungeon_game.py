51c8936a7a59075fc6ba6f30c133ba228b41c63c
teodoramorosan <tmorosan093@gmail.com>
2026-02-26 15:27:25 +0200

    feat: implement exploration and shop functionality in dungeon game

--- dungeon_game.py
+++ dungeon_game.py
@@ -93,3 +93,94 @@ def fighht(player):
 
 def explore(player):
     event = random.choice(["fight", "treasure", "nothing"])
+
+    if event == "fight":
+        fight(player)
+
+    elif event == "treasure":
+        gold_found = random.randint(15, 40)
+        plyer.gold += gold_found
+        print(f"💰 You found a treasure chest with {gold_found} gold!")
+
+        if random.random() > 0.6:
+            item = random.choice(["Magic Ring", "Health Potion","Iron Sword"])
+            player.inventory.append(item)
+            print(f"You also found: {item}")
+
+        else:
+            print("🌲 You explore but nothing happens...")
+
+
+        def shop(player):
+            print("\n🏪 Welcome to the shop")
+            print("1. Health Potion (20 gold)")
+            print("2. Upgrade Attack (+5) (50 gold)")
+            print("3. Exit")
+
+            choice = input("Choice: ")
+
+            if choice == "1":
+               if player.gold >= 20:
+                    player.gold -= 20
+                    player.hp += 30
+                    print("You used a Health Potion. +30 HP")
+               else:
+                    print("Not enough gold.")
+
+            elif choice == "2":
+                if player.gold >= 50:
+                    player.gold -= 50
+                    player.attack += 5
+                    print("Attack upgraded!")
+                else:
+                    print("Not enough gold.")
+    
+
+# ------------------ MAIN GAME LOOP ------------------
+
+def main():
+    slow_print("🏰 Welcome to AI Dungeon!")
+
+    name = input("Enter your name: ")
+    print("Choose role:")
+    print("1. Warrior")
+    print("2. Mage")
+    print("3. Rogue")
+
+    role_choice = input("Choice: ")
+
+    roles = {
+        "1": "Warrior",
+        "2": "Mange",
+        "3": "Rouge"
+    }
+
+    player = Player(name, roles.get(role_choice, "Rougue"))
+
+    while True:
+        player.show_stats()
+
+        print("1. Explore")
+        print("2. Shop")
+        print("3. Rest (+20 HP)")
+        print("4. Quit")
+
+        choice = input("Choice: ")
+
+        if choice == "1":
+            explore(player)
+        elif choice == "2":
+            shop(player)
+        elif choice == "3":
+            player.hp += 20
+            player.energy += 10 
+            print("You feel rested.")
+        elif choice == "4":
+            print("Goodbye adventurer!")
+            break
+        else:
+            print("Invalid option.")
+
+
+if  __name__ == "__main__":
+    main()
